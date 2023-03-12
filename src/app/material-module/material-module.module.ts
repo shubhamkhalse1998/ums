abc8728c9517f5e4097c272b948ccb480bc91593
shubham <shubhamkhalse12@gmail.com>
2023-03-12 07:16:53 -0700

    mmmm

--- src/app/material-module/material-module.module.ts
+++ src/app/material-module/material-module.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatDialogModule} from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon'
 
 
 
@@ -9,7 +10,9 @@ import {MatDialogModule} from '@angular/material/dialog';
 const components=[
   MatButtonModule,
   MatDatepickerModule,
-  MatDialogModule
+  MatDialogModule,
+  MatIconModule
+  
 ]
 
 
